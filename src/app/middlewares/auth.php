0af704ad1cf94a766fb53b06e400d1e276742371
Karl COSSE <dev@karlcosse.com>
2013-11-06 18:31:23 +0100

    Fix for global users

--- src/app/middlewares/auth.php
+++ src/app/middlewares/auth.php
@@ -2,7 +2,8 @@
 
 
 
-define('PROJECT_INDEX', 1);
+define('PLATEFORME_INDEX', 1);
+define('PROJECT_INDEX', 2);
 define('GLOBAL_PROJECT', 'global');
 class Auth extends \Slim\Middleware
 {
@@ -15,36 +16,43 @@ class Auth extends \Slim\Middleware
     public function call()
     {
         $url = $this->app->request()->getResourceUri();
-        if (substr($url, 0, 5) == '/apps') {
-            $folders = explode('/',substr($url,6));
+        $folders = explode('/', substr($url, 1));
+        if (sizeof((array)$folders) >= 3) {
+            //minimum folders = apps / ios / project
+            if ($folders[0] == 'apps') {
 
-            if (isset($_SERVER['PHP_AUTH_USER']) && isset($folders[PROJECT_INDEX])) {
-                $currentUser = $_SERVER['PHP_AUTH_USER'];
+                if (isset($_SERVER['PHP_AUTH_USER']) && isset($folders[PROJECT_INDEX])) {
+                    $currentUser = $_SERVER['PHP_AUTH_USER'];
 
-                $projects = array();
-                $currentProject = GLOBAL_PROJECT;
-                foreach (explode("\n", file_get_contents(DIR . '/.htpasswd')) as $k => $line) {
-                    if (trim($line) == '') {
-                        continue;
+                    $projects = array();
+                    $currentProject = GLOBAL_PROJECT;
+                    foreach (explode("\n", file_get_contents(DIR . '/.htpasswd')) as $k => $line) {
+                        if (trim($line) == '') {
+                            continue;
+                        }
+
+                        // Group found
+                        if (substr($line, 0, 1) == '#') {
+                            $currentProject = trim(str_replace('#', '', $line));
+                            continue;
+                        }
+
+                        // User found
+                        $user = substr($line, 0, strpos($line, ':'));
+                        $projects[$currentProject][] = $user;
                     }
 
-                    // Group found
-                    if (substr($line, 0, 1) == '#') {
-                        $currentProject = trim(str_replace('#', '', $line));
-                        continue;
+                    $project = $folders[PROJECT_INDEX];
+                    if (!in_array($currentUser, $projects[GLOBAL_PROJECT]) && (!isset($projects[$project]) || !in_array($currentUser, $projects[$project]))) {
+                        header('WWW-Authenticate: Basic realm="Projet protege"');
+                        header('HTTP/1.0 401 Unauthorized');
+                        $this->app->render('pages/unauthorized.twig', array(
+                                'parent_url' => toUrl(join('/', array_slice($folders, 0, sizeof($folders) - 1))),
+                                'parent' => $folders[sizeof($folders) - 2]
+                            )
+                        );
+                        return;
                     }
-
-                    // User found
-                    $user = substr($line, 0, strpos($line, ':'));
-                    $projects[$currentProject][] = $user;
-                }
-
-                $project = $folders[PROJECT_INDEX];
-                if ( !in_array($currentUser, $projects[GLOBAL_PROJECT]) && ( !isset($projects[$project]) || !in_array($currentUser, $projects[$project]))) {
-                    header('WWW-Authenticate: Basic realm="Projet protege"');
-                    header('HTTP/1.0 401 Unauthorized');
-                    $this->app->render('pages/unauthorized.twig');
-                    return;
                 }
             }
         }
