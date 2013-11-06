6bb6f159ed02307622f03d038a70504f9781069e
Karl COSSE <dev@karlcosse.com>
2013-11-06 18:48:24 +0100

    Fix security rules

--- src/app/middlewares/auth.php
+++ src/app/middlewares/auth.php
@@ -24,7 +24,7 @@ class Auth extends \Slim\Middleware
                 if (isset($_SERVER['PHP_AUTH_USER']) && isset($folders[PROJECT_INDEX])) {
                     $currentUser = $_SERVER['PHP_AUTH_USER'];
 
-                    $projects = array();
+                    $rules = array();
                     $currentProject = GLOBAL_PROJECT;
                     foreach (explode("\n", file_get_contents(DIR . '/.htpasswd')) as $k => $line) {
                         if (trim($line) == '') {
@@ -39,12 +39,33 @@ class Auth extends \Slim\Middleware
 
                         // User found
                         $user = substr($line, 0, strpos($line, ':'));
-                        $projects[$currentProject][] = $user;
+                        $rules[$currentProject][] = $user;
                     }
 
                     $project = $folders[PROJECT_INDEX];
-                    if (!in_array($currentUser, $projects[GLOBAL_PROJECT]) && (!isset($projects[$project]) || !in_array($currentUser, $projects[$project]))) {
-                        header('WWW-Authenticate: Basic realm="Projet protege"');
+
+                    if (empty($rules)) {
+                        //Allow from all
+                        $hasCredentials = true;
+                    } else {
+                        //Deny from all
+                        $hasCredentials = false;
+
+                        // currentUser is a project user
+                        if (isset($rules[$project]) && in_array($currentUser, $rules[$project])) {
+                            $hasCredentials = true;
+                        }
+
+                        // currentUser is a power user
+                        if (isset($rules[GLOBAL_PROJECT]) && in_array($currentUser, $rules[GLOBAL_PROJECT])) {
+                            $hasCredentials = true;
+                        }
+
+                    }
+
+
+                    if (!$hasCredentials) {
+                        header('WWW-Authenticate: Basic realm="Secured area"');
                         header('HTTP/1.0 401 Unauthorized');
                         $this->app->render('pages/unauthorized.twig', array(
                                 'parent_url' => toUrl(join('/', array_slice($folders, 0, sizeof($folders) - 1))),
