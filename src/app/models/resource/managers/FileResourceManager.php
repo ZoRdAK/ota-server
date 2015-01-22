970d6d6de98fd43688a94627286df9457d6ae592
Vincent Boesch <vincent.boesch@orange.com>
2015-01-22 09:21:25 +0100

    feature order files/folder by datetime descending

--- src/app/models/resource/managers/FileResourceManager.php
+++ src/app/models/resource/managers/FileResourceManager.php
@@ -9,7 +9,7 @@ class FileResourceManager implements ResourceManager
 	{
 		if (!file_exists($dir)) return true;
 		if (!is_dir($dir)) return unlink($dir);
-		foreach (scandir($dir) as $item) {
+		foreach (scan_dir($dir) as $item) {
 			if ($item == '.' || $item == '..') continue;
 			if (!static::deleteDirectory($dir . DIRECTORY_SEPARATOR . $item)) return false;
 		}
